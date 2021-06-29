71f90480fd1f4c6c04815b675abde8eb69c80700
Josh Oldes <jcoldes@outlook.com>
2021-06-28 22:59:48 -0700

    C++ 3D GT 19: Lighting module

--- DirectX11GameEngine/DirectX11GameEngine/VertexShader.hlsl
+++ DirectX11GameEngine/DirectX11GameEngine/VertexShader.hlsl
@@ -2,12 +2,15 @@ struct VS_INPUT
 {
 	float4 position: POSITION0;
 	float2 texcoord: TEXCOORD0;
+	float3 normal: NORMAL0;
 };
 
 struct VS_OUTPUT
 {
 	float4 position: SV_POSITION;
 	float2 texcoord: TEXCOORD0;
+	float3 normal: NORMAL0;
+	float3 direction_to_camera: TEXCOORD1;
 };
 
 cbuffer constant: register(b0)
@@ -15,7 +18,8 @@ cbuffer constant: register(b0)
 	row_major float4x4 m_world;
 	row_major float4x4 m_view;
 	row_major float4x4 m_proj;
-	unsigned int m_time;
+	float4 m_light_direction;
+	float4 m_camera_position;
 };
 
 VS_OUTPUT vsmain(VS_INPUT input)
@@ -25,7 +29,7 @@ VS_OUTPUT vsmain(VS_INPUT input)
 	//output.position = lerp(input.position, input.position1, (sin(m_time / 1000.0f) + 1.0f) / 2.0f);
 	// World Space
 	output.position = mul(input.position, m_world);
-
+	output.direction_to_camera = normalize(output.position.xyz - m_camera_position.xyz);
 	// View Space
 	output.position = mul(output.position, m_view);
 
@@ -33,6 +37,6 @@ VS_OUTPUT vsmain(VS_INPUT input)
 	output.position = mul(output.position, m_proj);
 
 	output.texcoord = input.texcoord;
-
+	output.normal = input.normal;
 	return output;
 }
